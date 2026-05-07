19ef11d884254134660e8d36b668e80e0b16ae8d
Mufaddal <mufaddalabbaskanchwala99@gmail.com>
2026-05-07 20:35:44 +0530

    fix config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # ======================================
-# RAZORPAY PAYMENT GATEWAY CONFIGURATION
+# APPLICATION CONFIGURATION
 # ======================================
-# Get these from: https://dashboard.razorpay.com/app/keys
-RAZORPAY_KEY_ID = "rzp_test_SaGwdxC2nNrJHg"        # Replace with your API key
-RAZORPAY_KEY_SECRET = "E0wo3EELRQ6U64QVFx0eL8T6"   # Replace with your API secret
+# Razorpay Payment Gateway API Keys
+
+RAZORPAY_KEY_ID = "rzp_test_SaGwdxC2nNrJHg"
+RAZORPAY_KEY_SECRET = "E0wo3EELRQ6U64QVFx0eL8T6"
